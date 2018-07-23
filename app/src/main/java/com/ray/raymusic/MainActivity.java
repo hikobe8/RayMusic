9c82de8c8b01871109747a024f070fec3e0927cb
hikobe8 <hikobe8@163.com>
2018-07-23 17:09:25 +0800

    optimize code

--- app/src/main/java/com/ray/raymusic/MainActivity.java
+++ app/src/main/java/com/ray/raymusic/MainActivity.java
@@ -2,7 +2,6 @@ package com.ray.raymusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.ray.musicplayer.Demo;
