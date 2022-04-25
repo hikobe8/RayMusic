59e80a54959be94bc628d67ed64002483dc4ac71
yurui <taia@zhumanggroup.com>
2022-04-25 14:59:00 +0800

    androidx

--- app/src/main/java/com/ray/app/MainActivity.java
+++ app/src/main/java/com/ray/app/MainActivity.java
@@ -1,6 +1,6 @@
 package com.ray.app;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
