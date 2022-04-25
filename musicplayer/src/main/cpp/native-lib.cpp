94eed8d7523ad83b981bcce716cd22fcadd3c994
hikobe8 <hikobe8@163.com>
2022-04-25 19:02:01 +0800

    import ffmpeg armeabi,arm64-v8a,x86

--- musicplayer/src/main/cpp/native-lib.cpp
+++ musicplayer/src/main/cpp/native-lib.cpp
@@ -1,10 +1,34 @@
 #include <jni.h>
 #include <string>
+#include "android/log.h"
+
+extern "C" {
+#include <libavformat/avformat.h>
+}
+
+#define LOGI(FORMAT, ...) __android_log_print(ANDROID_LOG_INFO,"hikobe8",FORMAT,##__VA_ARGS__);
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_ray_musicplayer_JniTest_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
+    av_register_all();
+    AVCodec *c_temp = av_codec_next(NULL);
+    while (c_temp != NULL) {
+        switch (c_temp->type) {
+            case AVMEDIA_TYPE_VIDEO:
+                LOGI("[Video]:%s", c_temp->name);
+                break;
+            case AVMEDIA_TYPE_AUDIO:
+                LOGI("[Audio]:%s", c_temp->name);
+                break;
+            default:
+                LOGI("[Other]:%s", c_temp->name);
+                break;
+        }
+        c_temp = c_temp->next;
+    }
+
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
--- musicplayer/src/main/java/com/ray/musicplayer/JniTest.java
+++ musicplayer/src/main/java/com/ray/musicplayer/JniTest.java
@@ -4,8 +4,14 @@ public class JniTest {
 
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("avutil-55");
+        System.loadLibrary("swresample-2");
+        System.loadLibrary("avcodec-57");
+        System.loadLibrary("avformat-57");
+        System.loadLibrary("swscale-4");
     }
 
-    public  native String stringFromJNI();
+
+    public native String stringFromJNI();
 
 }
